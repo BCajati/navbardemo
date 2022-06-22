8714905eac2fd27caeb3c5ab0c2de9b8547d6576
BCajati <BCajati@gmail.com>
2022-06-22 16:07:41 -0400

    Simple Bootstrap Navbar and Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavigationBar from './components/Navigation';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import 'react-bootstrap/dist/react-bootstrap.min.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavigationBar/>
+        <Routes>  
+        <Route  path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route  path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
